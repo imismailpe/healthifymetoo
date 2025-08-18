4b780cf439f159f36f7b61490d3a92c91795c70e
Ismail PE <mailismailpe@gmail.com>
2025-08-18 10:03:20 +0530

    fixed flash of sidebartrigger on mobile

--- components/site-header.tsx
+++ components/site-header.tsx
@@ -1,13 +1,19 @@
+"use client";
 import { Separator } from "@/components/ui/separator";
 import { SidebarTrigger } from "@/components/ui/sidebar";
 import { useIsMobile } from "@/hooks/use-mobile";
+import { useEffect, useState } from "react";
 
 export function SiteHeader({ title }: { title: string }) {
   const isMobile = useIsMobile();
+  const [loaded, setLoaded] = useState(false);
+  useEffect(() => {
+    setLoaded(true);
+  }, []);
   return (
     <header className="flex h-(--header-height) shrink-0 items-center gap-2 border-b transition-[width,height] ease-linear group-has-data-[collapsible=icon]/sidebar-wrapper:h-(--header-height)">
       <div className="flex w-full items-center gap-1 px-4 lg:gap-2 lg:px-6">
-        {isMobile ? null : (
+        {!loaded || isMobile ? null : (
           <>
             <SidebarTrigger className="-ml-1" />
             <Separator
